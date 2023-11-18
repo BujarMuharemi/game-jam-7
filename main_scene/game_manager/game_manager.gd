a70ad9be027bc7af3a971d9a88290250ff77879a
BujarMuharemi <bujar.muharemi.28@gmail.com>
2023-11-18 19:57:46 +0100

    added basics for pausing and options and the scene managing for that

--- main_scene/game_manager/game_manager.gd
+++ main_scene/game_manager/game_manager.gd
@@ -4,17 +4,18 @@ extends Node
 
 var start_scene
 var options_scene
+var pause_scene
+
 ## TODO: create independent btns which can be resued in all scenes !
-#TODO: make the options start etc just by showing and hiding stuff..avoid load of scenens and a lot of state managment
 
 func _ready():
-	options_scene = ResourceLoader.load("res://UI/options_scene/options_scene.tscn")
-	var option = options_scene.instantiate()
-	add_child(option)
-	print(option)
-	option.back_from_options_pressed.connect(back_from_option)
-	option.visible = false
+	options_scene = get_node("options_scene")
+	options_scene.back_from_options_pressed.connect(back_from_option)
 	
+	pause_scene = get_node("pause_scene")
+	pause_scene.continue_pressed.connect(continue_game)
+	pause_scene.show_options_pressed.connect(show_options)
+		
 	start_scene = get_node("start_scene")
 	start_scene.start_game_pressed.connect(start_game)
 	start_scene.options_pressed.connect(show_options)
@@ -24,26 +25,37 @@ func start_game():
 	print("game started")	
 	var main_scene = ResourceLoader.load("res://main_scene/main_scene.tscn")	
 	var main = main_scene.instantiate()
-	
-	# removing start scene and adding the main scene
-	start_scene.queue_free() 
 	add_child(main)
+	move_child(main,0)
 
-func show_options():	
-	var option_scene = ResourceLoader.load("res://UI/options_scene/options_scene.tscn")	
-	var option = option_scene.instantiate()
-	
-	# removing start scene and adding the main scene
-	start_scene.queue_free() 
-	add_child(option)
+	start_scene.visible = false 
+	start_scene.queue_free()
+
+func continue_game():
+	pause_scene.visible = false
+
+func show_options():
+	if(start_scene!=null):
+		start_scene.visible = false 
+	pause_scene.visible = false
+	options_scene.visible = true
+
+func show_pause_menu():
+	pause_scene.visible = true
 
 func back_from_option():
-	#var start_scene = ResourceLoader.load("res://UI/start_scene/start_scene.gd")	
-	#var start = start_scene.instantiate()
-	
-	get_node("options_scene").queue_free()
-	#add_child(start)
+	if(start_scene!=null):
+		start_scene.visible = true
+	if(!pause_scene.visible and start_scene==null):
+		pause_scene.visible = true
+	options_scene.visible = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _input(event):
+	#TODO: change it to an UI btn so it doen't close the full screen
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_ESCAPE:
+			if(start_scene==null and !options_scene.visible):
+				pause_scene.visible = !pause_scene.visible
+
+func _on_options_scene_back_from_options_pressed():
+	pass # Replace with function body.
